d6ea01f893e020c52b8fc9f4acbee6bc31ee0e2d
Archomeda <archomeda@hotmail.com>
2016-11-08 00:16:44 +0100

    Add !reset command

--- modules/general.js
+++ modules/general.js
@@ -2,6 +2,7 @@
 
 const
     config = require('config'),
+    moment = require('moment-timezone'),
     BaseModule = require('./base_module');
 
 class GeneralModule extends BaseModule {
@@ -67,6 +68,29 @@ class GeneralModule extends BaseModule {
                 return 'You can find the source code at https://github.com/Archomeda/raiders-inn-bot.';
             }
         };
+    }
+
+    cmd_resetTime() {
+        return {
+            id: config.get('modules.general.command_reset'),
+            help: 'Shows the current raid reset time and how much time there is left until the reset happens. This will also show a link to the wiki containing all Guild Wars 2 reset times.',
+            short_help: 'Shows the raid reset time',
+            on_command: () => {
+                const nextReset = moment().utc().day(8).hour(7).minute(30);
+                const timezones = [
+                    'America - Los Angeles',
+                    'America - New York',
+                    'Europe - London',
+                    'Europe - Paris',
+                    'Australia - Sydney',
+                    'Asia - Tokyo'
+                ];
+                return `Reset will happen ${moment().to(nextReset)}.\n\n` +
+                    `Raid rewards reset every ${nextReset.format('dddd [at] H:mm')} UTC. Other timezones:\n` +
+                    timezones.map(time => `:small_blue_diamond: ${time}: ${nextReset.clone().tz(time.replace(' - ', '/').replace(' ', '_')).format('dddd [at] H:mm')}`).join('\n') + '\n\n' +
+                    'For a full overview, check the wiki: https://wiki.guildwars2.com/wiki/Server_reset';
+            }
+        }
     }
 
     static formatCommandChannelFilter(command) {
