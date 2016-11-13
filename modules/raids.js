ec896e995ab59c8585b5b3ddbeb65d574f74fcde
Archomeda <archomeda@hotmail.com>
2016-11-13 07:14:51 +0100

    Fix !reset calculation

--- modules/raids.js
+++ modules/raids.js
@@ -20,7 +20,10 @@ class RaidsModule extends BaseModule {
             help: 'Shows the current raid reset time and how much time there is left until the reset happens. This will also show a link to the wiki containing all Guild Wars 2 reset times.',
             short_help: 'Shows the raid reset time',
             on_command: () => {
-                const nextReset = moment().utc().day(8).hour(7).minute(30);
+                const nextReset = moment().utc().day(1).hour(7).minute(30).seconds(0);
+                if (nextReset.isBefore(moment())) {
+                    nextReset.add(1, 'w');
+                }
                 const timezones = [
                     'America - Los Angeles',
                     'America - New York',
@@ -32,7 +35,7 @@ class RaidsModule extends BaseModule {
                 return `Reset will happen ${moment().to(nextReset)}.\n\n` +
                     `Raid rewards reset every ${nextReset.format('dddd [at] H:mm')} UTC. Other timezones:\n` +
                     timezones.map(time => `:small_blue_diamond: ${time}: ${nextReset.clone().tz(time.replace(' - ', '/').replace(' ', '_')).format('dddd [at] H:mm')}`).join('\n') + '\n\n' +
-                    'For a full overview, check the wiki: https://wiki.guildwars2.com/wiki/Server_reset';
+                    'For a complete reset overview, check the wiki: https://wiki.guildwars2.com/wiki/Server_reset';
             }
         }
     }
