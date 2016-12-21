68a1aa7255769c14e3b044c7f1c09c5302fc9edc
Archomeda <archomeda@hotmail.com>
2016-12-22 00:37:15 +0100

    Add i18n for !leave

--- modules/squads/CommandLeave.js
+++ modules/squads/CommandLeave.js
@@ -1,28 +1,34 @@
 'use strict';
 
 const
+    config = require('config'),
+    Promise = require('bluebird'),
+    i18next = Promise.promisifyAll(require('i18next')),
+
     CommandSquadBase = require('./CommandSquadBase'),
     CommandError = require('../../errors/CommandError');
 
 class CommandLeave extends CommandSquadBase {
     constructor(module) {
         super(module);
-
-        this.helpText = 'Leaves the current squad you are in. This only works in a squad channel.';
-        this.shortHelpText = 'Leaves your current squad';
+        i18next.loadNamespacesAsync('squads').then(() => {
+            this.helpText = i18next.t('squads:leave.help');
+            this.shortHelpText = i18next.t('squads:leave.short-help');
+        });
     }
 
     onCommand(response) {
         const squad = this.checkSquadChannel(response.message.channel);
         console.log(squad.leader, response.message.author.id);
         if (squad.leader === response.message.member.id) {
-            throw new CommandError(
-                "You can't leave the squad because you're the squad leader. " +
-                'You have to either transfer your squad leader status with `!transfer` or disband the squad entirely with `!disband`.'
-            );
+            throw new CommandError(i18next.t('squads:leave.response-leader', {
+                command_transfer: `${config.get('discord.command_prefix')}${this.module.config.commands.transfer.trigger}`,
+                command_disband: `${config.get('discord.command_prefix')}${this.module.config.commands.disband.trigger}`
+            }));
         }
         const textChannel = response.message.guild.channels.get(squad.textChannel);
-        return textChannel.sendMessage(`${response.message.member} has left the squad.`).then(() => squad.removeMember(response.message.member));
+        return textChannel.sendMessage(i18next.t('squads:leave.message-left', { member: response.message.member.toString() }))
+            .then(() => squad.removeMember(response.message.member));
     }
 }
 
