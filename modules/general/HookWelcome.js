d2cc0ec7ee242e842e4e102e02b2e5d4c5eb7622
Archomeda <archomeda@hotmail.com>
2016-12-21 16:42:54 +0100

    Fix welcome message string

--- modules/general/HookWelcome.js
+++ modules/general/HookWelcome.js
@@ -17,11 +17,11 @@ class HookWelcome extends DiscordHook {
             const welcomeChannel = member.guild.channels.get(this.config.channel);
             const targetChannel = member.guild.channels.get(this.config.target_channel);
             welcomeChannel.sendMessage(
-                ``${member}` has joined The Raiders Inn! Welcome! ` +
-                `Be sure to head over to ${targetChannel} to get started. ` +
-                'You will find our rules and general information there. ' +
-                `Don't forget to assign yourself to a server!\n\n` +
-                'Enjoy your stay! :beers:'
+                `${member} has joined The Raiders Inn! Welcome! 
+                Be sure to head over to ${targetChannel} to get started. 
+                You will find our rules and general information there. 
+                Don't forget to assign yourself to a server!\n\n
+                Enjoy your stay! :beers:`
             );
         } catch (err) {
             console.warn(`Caught error: ${err.message}`);
