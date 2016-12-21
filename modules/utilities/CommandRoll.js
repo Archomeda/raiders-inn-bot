c702641e1dd2c8d27fb4a36e3b92b92568a8f659
Archomeda <archomeda@hotmail.com>
2016-12-21 20:08:57 +0100

    Add i18n for !roll

--- modules/utilities/CommandRoll.js
+++ modules/utilities/CommandRoll.js
@@ -2,6 +2,8 @@
 
 const
     random = require('random-js')(),
+    Promise = require('bluebird'),
+    i18next = Promise.promisifyAll(require('i18next')),
 
     Command = require('../Command'),
     CommandParam = require('../CommandParam'),
@@ -10,13 +12,12 @@ const
 class CommandRoll extends Command {
     constructor(module) {
         super(module);
-
-        this.helpText = 'Rolls one or more dice and gives the result.';
-        this.shortHelpText = 'Rolls one or more dice';
-        this.params = [
-            new CommandParam('die', 'The die you want to use (d2 - d100)', true),
-            new CommandParam('rolls', 'The number of rolls (1 - 10)', true)
-        ];
+        i18next.loadNamespacesAsync('utilities').then(() => {
+            this.helpText = i18next.t('utilities:roll.help');
+            this.shortHelpText = i18next.t('utilities:roll.short-help');
+            this.params = new CommandParam('die', i18next.t('utilities:roll.param-die'), true);
+            this.params = new CommandParam('rolls', i18next.t('utilities:roll.param-rolls'), true);
+        });
     }
 
     onCommand(response) {
@@ -32,17 +33,17 @@ class CommandRoll extends Command {
             }
         }
         if (!die || die < 2 || die > this.config.roll_max_die) {
-            throw new CommandError('The die has to be between d2 and d100.');
+            throw new CommandError(i18next.t('utilities:roll.response-die-out-of-range'));
         }
         if (!rolls || rolls < 1 || rolls > this.config.roll_max_rolls) {
-            throw new CommandError('You can only roll between 1 and 10 times.');
+            throw new CommandError(i18next.t('utilities:roll.response-rolls-out-of-range'));
         }
 
         const result = random.dice(die, rolls);
 
         const generateSymbols = index => {
-            const black = ':black_medium_small_square:';
-            const white = ':white_medium_small_square:';
+            const black = '◾';
+            const white = '◽';
             return (
                 `${index % 4 === 0 ? black : white} ` +
                 `${index % 4 === 1 || index % 4 === 3 ? black : white} ` +
@@ -50,14 +51,23 @@ class CommandRoll extends Command {
             );
         };
 
-        response.message.channel.sendMessage(`${response.message.author} is rolling ${generateSymbols(0)}`).then(messageEdit => {
+        response.message.channel.sendMessage(i18next.t('utilities:roll.response-rolling', {
+            member: response.message.author.toString(),
+            symbols: generateSymbols(0)
+        })).then(messageEdit => {
             const update = ((message, result, receiver, i) => () => {
                 if (i < 5) {
-                    message.edit(`${receiver} is rolling ${generateSymbols(i)}`);
+                    message.edit(i18next.t('utilities:roll.response-rolling', {
+                        member: receiver.toString(),
+                        symbols: generateSymbols(i)
+                    }));
                     setTimeout(update, 600);
                     i++;
                 } else {
-                    message.edit(`${receiver} has rolled ${result.join(', ')}.`);
+                    message.edit(i18next.t('utilities:roll.response', {
+                        member: receiver.toString(),
+                        result: result.join(', ')
+                    }));
                 }
             })(messageEdit, result, response.message.author, 1);
             setTimeout(update, 600);
