3b933e681daba1dfad626757700d3333d6d5e5bf
Archomeda <archomeda@hotmail.com>
2016-12-22 00:30:41 +0100

    Add i18n for !requestsquad

--- modules/squads/CommandLeader.js
+++ modules/squads/CommandLeader.js
@@ -1,23 +1,27 @@
 'use strict';
 
 const
+    Promise = require('bluebird'),
+    i18next = Promise.promisifyAll(require('i18next')),
+
     CommandSquadBase = require('./CommandSquadBase'),
     CommandError = require('../../errors/CommandError');
 
 class CommandLeader extends CommandSquadBase {
     constructor(module) {
         super(module);
-
-        this.helpText = 'Gets the current leader of the squad. This only works in a squad channel.';
-        this.shortHelpText = 'Gets the current leader of the squad';
+        i18next.loadNamespacesAsync('squads').then(() => {
+            this.helpText = i18next.t('squads:leader.help');
+            this.shortHelpText = i18next.t('squads:leader.short-help');
+        });
     }
 
     onCommand(response) {
         const squad = this.checkSquadChannel(response.message.channel);
         if (!squad.leader) {
-            return 'This squad has currently no leader.';
+            return i18next.t('squads:leader.response-no-leader');
         }
-        return `This squad is led by ${response.message.guild.members.get(squad.leader)}.`;
+        return i18next.t('squads:leader.response', { leader: response.message.guild.members.get(squad.leader).toString() });
     }
 }
 
