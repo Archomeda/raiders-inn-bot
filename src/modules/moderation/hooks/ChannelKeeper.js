e24fac3fd1bf6264be8652ba5f46d736b879b6ed
Archomeda <archomeda@hotmail.com>
2017-10-17 08:32:44 +0200

    Fix duplicate channel creation and channel positioning

--- src/modules/moderation/hooks/ChannelKeeper.js
+++ src/modules/moderation/hooks/ChannelKeeper.js
@@ -19,7 +19,7 @@ class HookChannelKeeper extends DiscordHook {
 
     async keepChannels(origChannel) {
         const channelConfig = this.getConfig().get('channels');
-        const channelRegexes = channelConfig.map(r => new RegExp(r.replace('\\d', '(\\d+)')));
+        const channelRegexes = channelConfig.map(r => new RegExp(`^${r.replace('\\d', '(\\d+)')}$`));
 
         // Find all related channels
         const channels = [];
@@ -33,7 +33,7 @@ class HookChannelKeeper extends DiscordHook {
                 for (const channel of origChannel.guild.channels.filterArray(c => c.type === 'voice')) {
                     const match = channel.name.match(channelRegex);
                     if (match) {
-                        channels[parseInt(match[1], 10) - 1] = channel;
+                        channels.push({ channel, index: parseInt(match[1], 10) - 1 });
                     }
                 }
                 break;
@@ -42,17 +42,18 @@ class HookChannelKeeper extends DiscordHook {
                 return;
             }
         }
+        channels.sort((a, b) => a.index - b.index);
 
-        // Clean up and/or add new channels
-        let empty = channels.filter(c => c.members.size === 0).length;
+        let empty = channels.filter(c => c.channel.members.size === 0).length;
         if (empty > 2) {
+            // Delete some empty channels, starting with the last one
             for (let i = channels.length; i >= 0; i--) {
-                if (channels[i] && channels[i].members.size === 0) {
+                if (channels[i] && channels[i].channel.members.size === 0) {
                     try {
-                        await channels[i].delete('This channel is currently unused'); // eslint-disable-line no-await-in-loop
-                        this.log(`Removed unused channel ${channels[i].name}`, 'log');
+                        await channels[i].channel.delete('This channel is currently unused'); // eslint-disable-line no-await-in-loop
+                        this.log(`Removed unused channel ${channels[i].channel.name}`, 'log');
                     } catch (err) {
-                        this.log(`Error while removing unused channel ${channels[i].name}: ${err}`, 'warn');
+                        this.log(`Error while removing unused channel ${channels[i].channel.name}: ${err}`, 'warn');
                     }
                     empty--;
                 }
@@ -61,30 +62,52 @@ class HookChannelKeeper extends DiscordHook {
                 }
             }
         } else if (empty < 2) {
-            const channelBase = channels.find(c => c);
-            let pos = channelBase.position;
+            // Add some new channels
+            const channelBase = channels.find(c => c).channel;
+            const parentChannel = channelBase.parent;
+            const siblingChannels = parentChannel ? parentChannel.children.array() : channelBase.guild.channels.filterArray(c => !c.parent);
+            siblingChannels.sort((a, b) => a.position - b.position);
+            const numberOfChannelsToAdd = 2 - empty;
+
+            // 1) Change the positions of the channels allowing us to insert the new ones without position problems
+            const toOrder = [];
+            const startPos = siblingChannels.find(c => c.id === channels[0].channel.id).position;
+            for (const { channel, index } of channels) {
+                if (channel.position !== startPos + index) {
+                    toOrder.push({ channel: channel.id, position: startPos + index });
+                }
+            }
+            const siblingChannelsAfter = siblingChannels.filter(sc => sc.position >= startPos && !channels.find(c => c.channel.id === sc.id));
+            for (let i = 0; i < siblingChannelsAfter.length; i++) {
+                const channel = siblingChannelsAfter[i];
+                const pos = startPos + channels.length + i + numberOfChannelsToAdd;
+                if (channel.position !== pos) {
+                    toOrder.push({ channel: channel.id, position: pos });
+                }
+            }
+            await channelBase.guild.setChannelPositions(toOrder);
+
+            // 2) Add the new channels
             for (let i = 0; i < channels.length + 2; i++) {
-                if (!channels[i]) {
+                if (!channels.find(c => c.index === i)) {
+                    // Found a spot
                     const name = channelConf.replace('\\d', i + 1);
                     let newChannel;
                     try {
-                        pos++;
                         newChannel = await channelBase.clone({ name, reason: 'New channel to keep up with demand' }); // eslint-disable-line no-await-in-loop
                         const editData = {
-                            position: pos,
+                            position: startPos + i,
                             userLimit: channelBase.userLimit
                         };
                         if (channelBase.parent) {
                             editData.parentID = channelBase.parentID;
                         }
                         await newChannel.edit(editData); // eslint-disable-line no-await-in-loop
-                        this.log(`Created new channel ${name}`, 'log');
+                        this.log(`Created new channel ${name} at position ${editData.position}`, 'log');
                     } catch (err) {
                         this.log(`Error while creating new channel ${name}: ${err}`, 'warn');
                     }
                     empty++;
-                } else {
-                    pos = channels[i].position + 1;
                 }
                 if (empty >= 2) {
                     break;
@@ -117,8 +140,7 @@ class HookChannelKeeper extends DiscordHook {
                 }
                 this._timeout = undefined;
             }, waitTime);
-        }
-        if (oldChannel) {
+        } else if (oldChannel) {
             this._timeout = setTimeout(async () => {
                 try {
                     await this.keepChannels(oldChannel);
