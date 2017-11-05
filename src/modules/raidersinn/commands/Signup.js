cc237bb15bcad29afb288ba46f585dfff8178f18
Archomeda <archomeda@hotmail.com>
2017-11-06 00:13:27 +0100

    Fix emojis

--- src/modules/raidersinn/commands/Signup.js
+++ src/modules/raidersinn/commands/Signup.js
@@ -15,15 +15,19 @@ class CommandSignup extends DiscordCommand {
         const bot = this.getBot();
         const l = bot.getLocalizer();
         const ocnfig = this.getConfig();
+        const client = bot.getClient();
         const channelId = ocnfig.get('signup-channel-id');
         const roles = ocnfig.get('roles');
-        const channel = bot.getClient().channels.resolve(channelId);
+        const channel = client.channels.resolve(channelId);
 
         if (message.channel.type !== 'text') {
             return l.t('module.raidersinn:signup.response-no-dm');
         }
 
-        return l.t('module.raidersinn:signup.response', { channel, roles: roles.map(r => `${r.emoji} ${r.name}`).join('\n') });
+        return l.t('module.raidersinn:signup.response', {
+            channel,
+            roles: roles.map(r => `${client.emojis.resolve(r.emoji) || r.emoji} ${r.name}`).join('\n')
+        });
     }
 }
 
