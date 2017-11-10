01e66e4b4bd4b4f9a3dd7a6bba73bab6b8220111
Archomeda <archomeda@hotmail.com>
2017-11-10 19:58:37 +0100

    Fix the channel order again, hopefully for good this time

--- src/modules/moderation/hooks/ChannelKeeper.js
+++ src/modules/moderation/hooks/ChannelKeeper.js
@@ -17,43 +17,34 @@ class HookChannelKeeper extends DiscordHook {
         };
     }
 
-    async keepChannels(origChannel) {
-        const channelConfig = this.getConfig().get('channels');
-        const channelRegexes = channelConfig.map(r => new RegExp(`^${r.replace('\\d', '(\\d+)')}$`));
+    async keepChannelsOfGroup(guild, groupConf) {
+        const groupRegex = new RegExp(`^${groupConf.replace('\\d', '(\\d+)')}$`);
 
         // Find all related channels
+        const groupChannels = guild.channels.filterArray(c => c.type === 'voice' && groupRegex.test(c.name));
         const channels = [];
-        let channelRegex;
-        let channelConf;
-        for (let i = 0; i < channelConfig.length; i++) {
-            channelRegex = channelRegexes[i];
-            channelConf = channelConfig[i];
-            const match = origChannel.name.match(channelRegex);
-            if (match) {
-                for (const channel of origChannel.guild.channels.filterArray(c => c.type === 'voice')) {
-                    const match = channel.name.match(channelRegex);
-                    if (match) {
-                        channels.push({ channel, index: parseInt(match[1], 10) - 1 });
-                    }
-                }
-                break;
-            } else if (channelConfig.length === i + 1) {
-                // No match
-                return;
+        for (const channel of groupChannels) {
+            const match = channel.name.match(groupRegex);
+            const index = parseInt(match[1], 10) - 1;
+            if (index >= 0) {
+                channels[index] = channel;
             }
         }
-        channels.sort((a, b) => a.index - b.index);
 
-        let empty = channels.filter(c => c.channel.members.size === 0).length;
+        let empty = groupChannels.filter(c => c.members.size === 0).length;
         if (empty > 2) {
-            // Delete some empty channels, starting with the last one
+            // Delete some empty channels until we have 2 left
+            const exec = [];
             for (let i = channels.length; i >= 0; i--) {
-                if (channels[i] && channels[i].channel.members.size === 0) {
+                if (!channels[i]) {
+                    continue;
+                }
+                if (channels[i].members.size === 0) {
                     try {
-                        await channels[i].channel.delete('This channel is currently unused'); // eslint-disable-line no-await-in-loop
-                        this.log(`Removed unused channel ${channels[i].channel.name}`, 'log');
+                        exec.push(channels[i].delete('Unused channel'));
+                        this.log(`Removed unused channel ${channels[i].name}`, 'log');
                     } catch (err) {
-                        this.log(`Error while removing unused channel ${channels[i].channel.name}: ${err}`, 'warn');
+                        this.log(`Error while removing unused channel ${channels[i].name}: ${err}`, 'warn');
                     }
                     empty--;
                 }
@@ -61,49 +52,56 @@ class HookChannelKeeper extends DiscordHook {
                     break;
                 }
             }
+            await Promise.all(exec);
         } else if (empty < 2) {
             // Add some new channels
-            const channelBase = channels.find(c => c).channel;
+            const channelBase = groupChannels[0];
+            if (!channelBase) {
+                this.log('Error while creating new channels: No base channel can be found', 'warn');
+                return;
+            }
             const parentChannel = channelBase.parent;
-            const siblingChannels = parentChannel ? parentChannel.children.array() : channelBase.guild.channels.filterArray(c => !c.parent);
-            siblingChannels.sort((a, b) => a.position - b.position);
             const numberOfChannelsToAdd = 2 - empty;
 
-            // 1) Change the positions of the channels allowing us to insert the new ones without position problems
+            // 1. Predetermine the channel order
             const toOrder = [];
-            const startPos = siblingChannels.find(c => c.id === channels[0].channel.id).position;
-            for (const { channel, index } of channels) {
-                if (channel.position !== startPos + index) {
-                    toOrder.push({ channel: channel.id, position: startPos + index });
+            const groupSiblingChannels = parentChannel ? parentChannel.children.array() : guild.channels.filterArray(c => !c.parent);
+            const siblingChannels = [];
+            for (const channel of groupSiblingChannels) {
+                siblingChannels[channel.position] = channel;
+            }
+            const startPos = groupSiblingChannels.find(c => c.id === channels.find(c => c).id).position;
+            let endPos = startPos;
+            for (let i = 0; i < channels.length; i++) {
+                if (channels[i]) {
+                    toOrder.push({ channel: channels[i].id, position: startPos + i });
+                    endPos = startPos + i;
                 }
             }
-            const siblingChannelsAfter = siblingChannels.filter(sc => sc.position >= startPos && !channels.find(c => c.channel.id === sc.id));
-            for (let i = 0; i < siblingChannelsAfter.length; i++) {
-                const channel = siblingChannelsAfter[i];
-                const pos = startPos + channels.length + i + numberOfChannelsToAdd;
-                if (channel.position !== pos) {
-                    toOrder.push({ channel: channel.id, position: pos });
+            endPos += 1 + numberOfChannelsToAdd;
+            for (let i = 0; i < siblingChannels.length; i++) {
+                if (i < startPos) {
+                    toOrder.push({ channel: siblingChannels[i].id, position: i });
+                } else if (siblingChannels[i] && !channels.some(c => c.id === siblingChannels[i].id)) {
+                    toOrder.push({ channel: siblingChannels[i].id, position: endPos });
+                    endPos++;
                 }
             }
-            await channelBase.guild.setChannelPositions(toOrder);
 
-            // 2) Add the new channels
+            // 2. Create the channels
             for (let i = 0; i < channels.length + 2; i++) {
-                if (!channels.find(c => c.index === i)) {
+                if (!channels[i]) {
                     // Found a spot
-                    const name = channelConf.replace('\\d', i + 1);
+                    const name = groupConf.replace('\\d', i + 1);
                     let newChannel;
                     try {
                         newChannel = await channelBase.clone({ name, reason: 'New channel to keep up with demand' }); // eslint-disable-line no-await-in-loop
-                        const editData = {
-                            position: startPos + i,
-                            userLimit: channelBase.userLimit
-                        };
-                        if (channelBase.parent) {
-                            editData.parentID = channelBase.parentID;
-                        }
-                        await newChannel.edit(editData); // eslint-disable-line no-await-in-loop
-                        this.log(`Created new channel ${name} at position ${editData.position}`, 'log');
+                        await newChannel.edit({ // eslint-disable-line no-await-in-loop
+                            parentID: channelBase.parentID,
+                            userLimit: channelBase.userLimit // Somehow this doesn't get cloned or something...
+                        });
+                        toOrder.push({ channel: newChannel.id, position: startPos + i });
+                        this.log(`Created new channel ${name} at position ${startPos + i}`, 'log');
                     } catch (err) {
                         this.log(`Error while creating new channel ${name}: ${err}`, 'warn');
                     }
@@ -113,6 +111,16 @@ class HookChannelKeeper extends DiscordHook {
                     break;
                 }
             }
+
+            // 3. Properly order the channels
+            await guild.setChannelPositions(toOrder);
+        }
+    }
+
+    async keepChannels(guild) {
+        const channelConfig = this.getConfig().get('channels');
+        for (const conf of channelConfig) {
+            await this.keepChannelsOfGroup(guild, conf); // eslint-disable-line no-await-in-loop
         }
     }
 
@@ -122,7 +130,6 @@ class HookChannelKeeper extends DiscordHook {
         }
 
         const channels = this.getConfig().get('channels');
-
         if (channels.length === 0) {
             // No channels
             return;
@@ -130,20 +137,11 @@ class HookChannelKeeper extends DiscordHook {
 
         const oldChannel = oldMember.voiceChannel;
         const newChannel = newMember.voiceChannel;
-
-        if (newChannel) {
+        const guild = oldChannel ? oldChannel.guild : newChannel ? newChannel.guild : undefined;
+        if (guild) {
             this._timeout = setTimeout(async () => {
                 try {
-                    await this.keepChannels(newChannel);
-                } catch (err) {
-                    this.log(`Error while keeping channels: ${err}`, 'warn');
-                }
-                this._timeout = undefined;
-            }, waitTime);
-        } else if (oldChannel) {
-            this._timeout = setTimeout(async () => {
-                try {
-                    await this.keepChannels(oldChannel);
+                    await this.keepChannels(guild);
                 } catch (err) {
                     this.log(`Error while keeping channels: ${err}`, 'warn');
                 }
