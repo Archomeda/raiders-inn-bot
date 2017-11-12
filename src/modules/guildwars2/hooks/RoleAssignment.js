66a4279c220ddc1e92a90740602b1e159ea4e647
Archomeda <archomeda@hotmail.com>
2017-11-12 20:23:04 +0100

    Change profession roles to generic raid roles

--- src/modules/guildwars2/hooks/RoleAssignment.js
+++ src/modules/guildwars2/hooks/RoleAssignment.js
@@ -3,12 +3,9 @@
 const DiscordHook = require('../../../../bot/modules/DiscordHook');
 
 
-const professions = ['guardian', 'revenant', 'warrior', 'engineer', 'ranger', 'thief', 'elementalist', 'mesmer', 'necromancer'];
-
-
-class HookProfessionAssignment extends DiscordHook {
+class HookRoleAssignment extends DiscordHook {
     constructor(bot) {
-        super(bot, 'profession-assignment');
+        super(bot, 'role-assignment');
         this._hooks = {
             messageReactionAdd: this.onAdd.bind(this),
             messageReactionRemove: this.onRemove.bind(this)
@@ -42,33 +39,33 @@ class HookProfessionAssignment extends DiscordHook {
             return;
         }
 
+        const roleAssignments = config.get('roles');
         const emojiToRole = new Map();
-        for (const profession of professions) {
-            const professionConfig = config.get(profession);
-            emojiToRole.set(professionConfig.get('emoji-id'), professionConfig.get('role-id'));
+        for (const role of roleAssignments) {
+            emojiToRole.set(role['emoji-id'], role['role-id']);
         }
 
         const raiderRoleId = moduleConfig.get('raider.enabled') ? moduleConfig.get('raider.role-id') : undefined;
         const roleId = emojiToRole.get(reaction.emoji.id || reaction.emoji.name);
         if (roleId && guild.roles.has(roleId)) {
             if (isAdded) {
-                // Apply profession
+                // Apply roles
                 if (!member.roles.has(roleId)) {
-                    await member.addRole(roleId, 'Profession assignment applied via message reaction');
+                    await member.addRole(roleId, 'Raid role assignment applied via message reaction');
                 }
                 // Apply raider
                 if (raiderRoleId && guild.roles.has(raiderRoleId) && !member.roles.has(raiderRoleId)) {
-                    await member.addRole(raiderRoleId, 'Profession assignment implies raider role');
+                    await member.addRole(raiderRoleId, 'Raid role assignment implies raider role');
                 }
             } else {
-                // Remove profession
+                // Remove raid role
                 if (member.roles.has(roleId)) {
-                    await member.removeRole(roleId, 'Profession assignment removed via message reaction');
+                    await member.removeRole(roleId, 'Raid role assignment removed via message reaction');
                 }
-                // Remove raider if no profession roles have been assigned
+                // Remove raider if no raid roles have been assigned
                 const roleIds = Array.from(emojiToRole.values());
                 if (member.roles.filterArray(r => roleIds.includes(r.id)).length === 0) {
-                    await member.removeRole(raiderRoleId, 'No profession assignments implies no raider role');
+                    await member.removeRole(raiderRoleId, 'No raid role assignments implies no raider role');
                 }
             }
         } else if (isAdded) {
@@ -92,31 +89,34 @@ class HookProfessionAssignment extends DiscordHook {
         if (channel) {
             const message = await channel.messages.fetch(config.get('message-id'));
             if (message) {
-                const emojis = professions.map(p => config.get(`${p}.emoji-id`));
+                const roleAssignments = config.get('roles');
+                if (roleAssignments && roleAssignments.length > 0) {
+                    const emojis = roleAssignments.map(r => r['emoji-id']);
 
-                // Apply our reactions to it if they don't exist yet
-                for (const emoji of emojis) {
-                    const reaction = message.reactions.get(emoji) || message.reactions.find('id', emoji) || message.reactions.find('name', emoji);
-                    if (!reaction || !reaction.me) {
-                        try {
-                            await message.react(emoji); // eslint-disable-line no-await-in-loop
-                        } catch (err) {
-                            // Reacting failed, possibly because the emoji doesn't exist
-                            this.log(`Reacting on profession assignment message failed: ${err}`, 'warn');
+                    // Apply our reactions to it if they don't exist yet
+                    for (const emoji of emojis) {
+                        const reaction = message.reactions.get(emoji) || message.reactions.find('id', emoji) || message.reactions.find('name', emoji);
+                        if (!reaction || !reaction.me) {
+                            try {
+                                await message.react(emoji); // eslint-disable-line no-await-in-loop
+                            } catch (err) {
+                                // Reacting failed, possibly because the emoji doesn't exist
+                                this.log(`Reacting on profession assignment message failed: ${err}`, 'warn');
+                            }
                         }
                     }
-                }
 
-                // Remove unsupported reactions
-                const exec = [];
-                for (const reaction of message.reactions.filterArray(r => !emojis.includes(r.emoji.id) && !emojis.includes(r.emoji.name))) {
-                    // .fetchUsers() is hardcapped at 100 users at a time
-                    const users = (await reaction.fetchUsers()).keyArray(); // eslint-disable-line no-await-in-loop
-                    for (const user of users) {
-                        exec.push(reaction.remove(user));
+                    // Remove unsupported reactions
+                    const exec = [];
+                    for (const reaction of message.reactions.filterArray(r => !emojis.includes(r.emoji.id) && !emojis.includes(r.emoji.name))) {
+                        // .fetchUsers() is hardcapped at 100 users at a time
+                        const users = (await reaction.fetchUsers()).keyArray(); // eslint-disable-line no-await-in-loop
+                        for (const user of users) {
+                            exec.push(reaction.remove(user));
+                        }
                     }
+                    await Promise.all(exec);
                 }
-                await Promise.all(exec);
             }
         }
 
@@ -124,4 +124,4 @@ class HookProfessionAssignment extends DiscordHook {
     }
 }
 
-module.exports = HookProfessionAssignment;
+module.exports = HookRoleAssignment;
